60b9e8e09c1ae35742042f3ba10a073876846a2c
quanpham1111 <nguyenquan10k4@gmail.com>
2024-01-25 03:16:23 -0800

    video10

--- js/video10/video10.js
+++ js/video10/video10.js
@@ -2,5 +2,6 @@
 let a=5;
 let b='5';
 console.log(`so sanh a va b :`, `a=`,a,`b=`,b,`a = b :`,a===b);
-// == khong quan trong type(interger), chi so sanh value
-//type, value
+// == :khong quan trong type(interger), chi so sanh value
+//=== :type, value
+//update
