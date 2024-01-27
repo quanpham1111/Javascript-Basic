a2813a109cbce4d9d4575c44210d6acee7c80264
quanpham1111 <nguyenquan10k4@gmail.com>
2024-01-26 20:10:37 -0800

    for loop

---
+++ js/video11/video11.js
@@ -0,0 +1,10 @@
+console.log(`hello world from javascript`);
+for(let i=1;i<=10;i++){
+    console.log(`[`,i,`]`,`hello world`);
+}
+//những cây vợt hot với array
+console.log(`Những cây vợt hot năm 2023:`);
+let arr =[`kumpoo`,`Vs`,`Kamito`,`Yonex`];
+for(let i=0;i<arr.length;i++ ){
+    console.log(`Top `,i +1,`: `,arr[i]);
+}
