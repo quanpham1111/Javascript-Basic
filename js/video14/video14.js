df05716914b0c8d2b4c08384f58d7ea9491ea0aa
Pham Nguyen Quan <Pham Nguyen Quan@DESKTOP-HDC4IAA>
2024-03-24 06:04:23 -0700

    variable & block code

---
+++ js/video14/video14.js
@@ -0,0 +1,14 @@
+//variables & block scope: biến và phạm vi sử dụng
+//global
+//Block
+//console.log('value x :  ',x)
+//var x=5;
+//block code
+for(let i=0;i <10;i++ ){
+    console.log('check value i:',i)
+}
+//global code
+for(var i=0;i <10;i++ ){
+    console.log('check value i:',i)
+}
+console.log('check value i gobal:',i)
