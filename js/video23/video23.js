ff1d7f93243ad7acaea1a320cf86136f734446e9
Nguyen Quan <nguyenquan10k4@gmail.com>
2024-12-25 22:21:35 +0700

    Array

--- js/video23/video23.js
+++ js/video23/video23.js
@@ -3,3 +3,7 @@ const arr= [ 1,1000,20,4];
 arr.sort((item1,item2) =>{ console.log(item1,item2)
     return item2 -item1; } ); 
     console.log(` sắp xếp mảng theo thứ tự tăng dần:`,arr);
+//Another way
+const array=[1,10000,20,4];
+array.sort((item1,item2)=> item1 -item2);
+console.log(array);
